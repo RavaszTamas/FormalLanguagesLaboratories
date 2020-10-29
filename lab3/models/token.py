d7a4bcd151bc9add6648d17a8983bd525110162d
RavaszTamas <rtie2614@scs.ubbcluj.ro>
2020-10-29 18:16:11 +0200

    small change

--- lab3/models/token.py
+++ lab3/models/token.py
@@ -10,7 +10,7 @@ reservedWords = ['ha', 'kulonben', 'karakter', 'karakterlanc', 'amig', 'boolean'
 
 all_items = separators + operators + reservedWords
 codification = dict([(all_items[i], i + 2) for i in range(len(all_items))])
-codification['identifier'] = 0
-codification['constant'] = 1
+codification['constant'] = 0
+codification['identifier'] = 1
 
 alphabet = list(string.ascii_lowercase) + list(string.ascii_uppercase) + list(string.ascii_letters) + ['_']
