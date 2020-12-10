834ba5235dc31911643a18d01f199ec676b5d13d
RavaszTamas <rtie2614@scs.ubbcluj.ro>
2020-12-10 18:49:17 +0200

    Final commit of lab 5-6-7

--- lab5-7/venv/Scripts/activate.bat
+++ lab5-7/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ravas\Documents\UBB\CompNet\lab5\venv
+set VIRTUAL_ENV=C:\Users\ravas\Documents\UBB\FLCD_github\FormalLanguagesLaboratories\lab5-7\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
